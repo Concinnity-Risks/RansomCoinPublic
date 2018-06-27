2f0d3b9cfb1ff81354a3fc21546b15f5797e6da2
blackswanburst <eireann.leverett@cantab.net>
2018-06-27 13:06:50 +0300

    Added yield assesment, and removed another potential observer effect.

--- Tools/coinlector.py
+++ Tools/coinlector.py
@@ -56,12 +56,14 @@ def validate_xmr_address(xmr_address):
     try:
         a = address(xmr_address)
         return True
-    except ValueError:
+    except:
         return False
 #Section for regexes we're interested in
 
 #URLs
 url = re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")
+#Onion addresses
+onion = re.compile("(?:https?://)|(?:http?://)?(?:www)?(\S*?\.onion)\b")
 
 #Crypto currency addresses, bank accounts, and pay ids
 btc_priv_key = re.compile("5[HJK][1-9A-Za-z][^OIl]{48}")
@@ -78,12 +80,14 @@ try:
 except OSError:
     pass
 
-#The main body of code
+#This variable just helps us track how many malware files we have potential Monetisation IoCs for
+CoinsCollected = 0
+
 with open('Ransomware.csv', 'wb') as csvfile:
     resultswriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     resultswriter.writerow(['md5','sha1','sha256','filename','Class of Observable','Address'])
     for filename in tqdm(os.listdir(os.getcwd())):
-        if filename == 'Ransomware.csv':
+        if filename == 'Ransomware.csv' or filename == 'coinlector.py':
             pass
         else:
             with open(filename, 'rb') as f:
@@ -93,23 +97,39 @@ with open('Ransomware.csv', 'wb') as csvfile:
                 sha1 =  hashlib.sha1(readFile).hexdigest()
                 sha256 =  hashlib.sha256(readFile).hexdigest()
                 openedFile.close()
+                CoinCollected = False
                 for line in tqdm(process(f)):
                     if url.search(line):
                         resultswriter.writerow([md5,sha1,sha256,filename,"URL",url.search(line).group(0)])
+                        if onion.search(url.search(line).group(0)) and CoinCollected == False:
+                            CoinCollected = True
                     elif btc_priv_key.search(line) and b58decode_check(btc_priv_key.search(line).group(0)) == True:
                         resultswriter.writerow([md5,sha1,sha256,filename,"Bitcoin Private Key",btc_priv_key.search(line).group(0)])
+                        if CoinCollected == False:
+                            CoinCollected = True
                     elif xmr.search(line) and validate_xmr_address(xmr.search(line).group(0)):
                         resultswriter.writerow([md5,sha1,sha256,filename,"XMR Address",xmr.search(line).group(0)])
+                        if CoinCollected == False:
+                            CoinCollected = True
                     elif email.search(line):
                         resultswriter.writerow([md5,sha1,sha256,filename,"Email Address",email.search(line).group(0)])
+                        if CoinCollected == False:
+                            CoinCollected = True
                     #This one needs to be near the bottom, as it matches shorter base58 strings
                     elif btcorbch.search(line) and b58decode_check(btcorbch.search(line).group(0)):
                         resultswriter.writerow([md5,sha1,sha256,filename,"BTC/BCH Address",btcorbch.search(line).group(0)])
+                        if CoinCollected == False:
+                            CoinCollected = True
                     #This one needs to be last, as it basically matches domains and emails too
                     elif xmrpayid.search(line):
                         resultswriter.writerow([md5,sha1,sha256,filename,"XMR Pay ID",xmrpayid.search(line).group(0)])
+                        if CoinCollected == False:
+                            CoinCollected = True
                     else:
                         pass
             f.close()
+            if CoinCollected == True:
+                CoinsCollected += 1
 csvfile.close()
 print "\n"
+print "Yield ratio is: "+str(100*CoinsCollected/len(os.listdir(os.getcwd())))+"%\n"
