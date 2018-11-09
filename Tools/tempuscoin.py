70475c99cb8a064097065565722175cd3516cb16
blackswanburst <eireann.leverett@cantab.net>
2018-11-09 16:49:53 +0000

    GNU licenses removed and tempuscoin.py added

--- Tools/tempuscoin.py
+++ Tools/tempuscoin.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python
 # Copyleft 2018 Eireann Leverett of Concinnity Risks
 #
-#
-# File name chasingcoin.py
+# File name tempuscoin.py
 # written by eireann.leverett@cantab.net
 import requests
 import json
 import csv
 from tqdm import tqdm
+import sys
+import time
 
 LIST_OF_ADDRESSES = []
 BATCH = ''
 
-with open('Ransomware.csv', 'rb') as inputfile:
-    coinreader = csv.reader(inputfile, delimiter=',',)
-    for row in coinreader:
-        if row[4] == 'BTC/BCH Address':
-            LIST_OF_ADDRESSES.append(row[5])
+if len(sys.argv) < 0:
+    with open(sys.argv[1], 'rb') as inputfile:
+        #We assume it is a text file with BTC addresses on each line
+        for line in inputfile:
+            LIST_OF_ADDRESSES.append(line)
+else:
+    with open('Ransomware.csv', 'rb') as inputfile:
+        coinreader = csv.reader(inputfile, delimiter=',',)
+        for row in coinreader:
+            if row[4] == 'BTC/BCH Address':
+                LIST_OF_ADDRESSES.append(row[5])
 inputfile.close()
 
 # deduplicate the list
@@ -33,7 +40,7 @@ with open('AccountsRecievingRansom.csv', 'wb') as csvfile:
                              'number of transactions',
                              'total received',
                              'total sent',
-                             'final balance'])
+                             'final balance', 'transactions'])
     for ADDRESS in tqdm(LIST_OF_ADDRESSES):
         #BATCH += ADDRESS + '|'
         result = requests.get('https://blockchain.info/rawaddr/' + ADDRESS)
@@ -43,7 +50,9 @@ with open('AccountsRecievingRansom.csv', 'wb') as csvfile:
                                      data['n_tx'],
                                      data['total_received'],
                                      data['total_sent'],
-                                     data['final_balance']])
+                                     data['final_balance'],
+                                     data['txs']])
+            time.sleep(0.1)
         else:
             print 'HTTP Response is: ' + str(result.status_code)
 csvfile.close()
